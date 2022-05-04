21911951ff2ff1cdcc665c5d6c5f9d26139b6a99
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-05-04 19:05:40 +0200

    feat: support github packages for publishing

--- src/publishing.ts
+++ src/publishing.ts
@@ -8,14 +8,34 @@ export type PublishOptions = {
   namespace: string;
   registries: Registry[];
 };
-type Mutable<T> = {
-  -readonly [k in keyof T]: T[k];
-};
+
+type AtomicObject =
+  | Function
+  | Promise<any>
+  | Date
+  | RegExp
+  | Boolean
+  | Number
+  | String;
+
+type Mutable<T> = T extends AtomicObject
+  ? T
+  : T extends ReadonlyMap<infer K, infer V> // Map extends ReadonlyMap
+  ? Map<Mutable<K>, Mutable<V>>
+  : T extends ReadonlySet<infer V> // Set extends ReadonlySet
+  ? Set<Mutable<V>>
+  : T extends object
+  ? { -readonly [K in keyof T]: Mutable<T[K]> }
+  : T;
 
 type Config = Mutable<
   Pick<
     ConstructLibraryOptions,
-    "publishToPypi" | "publishToMaven" | "publishToNuget" | "releaseToNpm"
+    | "publishToPypi"
+    | "publishToMaven"
+    | "publishToNuget"
+    | "releaseToNpm"
+    | "npmRegistryUrl"
   >
 >;
 
@@ -33,6 +53,7 @@ export function publishToRegistries(options: PublishOptions): Config {
       module: `${sanitizedNamespace}_${sanitizedName}`,
     };
   }
+
   if (registries.includes("maven")) {
     config.publishToMaven = {
       javaPackage: `com.${sanitizedNamespace}.${sanitizedName}`,
@@ -40,6 +61,7 @@ export function publishToRegistries(options: PublishOptions): Config {
       mavenArtifactId: sanitizedName,
     };
   }
+
   if (registries.includes("nuget")) {
     const nugetName = `${pascalCase(namespace)}.${pascalCase(name)}`;
     config.publishToNuget = {
@@ -47,5 +69,29 @@ export function publishToRegistries(options: PublishOptions): Config {
       packageId: nugetName,
     };
   }
+  return config;
+}
+
+export function publishToGithubPackages(options: PublishOptions): Config {
+  const { registries } = options;
+  const config = publishToRegistries(options);
+  if (registries.includes("pypi")) {
+    throw new Error("Github Packages does not support python packages");
+  }
+  if (registries.includes("nuget")) {
+    // Seems like projen does not support github packages on nuget yet: https://github.dev/projen/projen/blob/14f37ec704afdc5143e6a2954c1250b1f0ccaddf/src/release/publisher.ts#L343
+    throw new Error(
+      "Github Packages does support nuget packages, but this library does not yet support it"
+    );
+  }
+
+  if (registries.includes("npm")) {
+    config.npmRegistryUrl = "npm.pkg.github.com";
+  }
+
+  if (registries.includes("maven")) {
+    config.publishToMaven!.mavenRepositoryUrl = "https://maven.pkg.github.com";
+  }
+
   return config;
 }
