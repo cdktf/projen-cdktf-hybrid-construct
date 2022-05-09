a21f441ef7f45c9ee06111e1ad4bef8f90737098
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-05-09 11:43:22 +0200

    feat: allow to disable terraform plan (#66)

--- examples/hybrid-module/scripts/tf-module-test.sh
+++ examples/hybrid-module/scripts/tf-module-test.sh
@@ -1,10 +1,9 @@
+#!/bin/bash
+# This script is created by projen, do not edit it directly.
+set -e
 
-  #!/bin/bash
-  # This script is created by projen, do not edit it directly.
-  set -e
-  
-  terraform -chdir=terraform init --upgrade
-  terraform -chdir=terraform fmt
-  terraform -chdir=terraform validate
-  terraform -chdir=terraform plan     
+terraform -chdir=terraform init --upgrade
+terraform -chdir=terraform fmt
+terraform -chdir=terraform validate
+
           
