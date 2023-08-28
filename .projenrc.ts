162e3e07ccee41968eb8f822a759a4dd59425f89
Nara Kasbergen <nara.kasbergen@gmail.com>
2023-08-28 11:39:16 +0200

    chore: attempt 2

--- .projenrc.ts
+++ .projenrc.ts
@@ -99,10 +99,12 @@ project.addTask("upgrade:terraform", {
   cwd: "./examples/terraform-module",
 });
 project.removeTask("post-upgrade");
-project.addTask("post-upgrade", {
+const upgradeTask = project.addTask("post-upgrade", {
   description: "Runs after upgrading dependencies",
-  exec: "yarn compile && yarn projen upgrade:hybrid && yarn projen upgrade:terraform",
+  exec: "yarn build",
 });
+upgradeTask.exec("yarn projen upgrade:hybrid");
+upgradeTask.exec("yarn projen upgrade:terraform");
 
 project.testTask.exec("yarn buildExample");
 
