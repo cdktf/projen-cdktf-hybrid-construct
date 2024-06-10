5cebe5fa9839864815772474552a850fcfb91745
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-06-10 00:25:07 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- examples/hybrid-module/terraform/provider.tf
+++ examples/hybrid-module/terraform/provider.tf
@@ -1,19 +1,19 @@
 # This file is managed by projen. Do not edit, change the .projenrc file instead.
 
-    terraform {
-      # Limit provider version (some modules are not compatible with aws 4.x)
-      required_providers {
-        aws = {
-          source  = "hashicorp/aws"
-          version = "~> 3.74"
-        }
-      }
-      # Terraform binary version constraint
-      required_version = ">= 1.2.0"
+terraform {
+  # Limit provider version (some modules are not compatible with aws 4.x)
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.74"
     }
-    
-    
-    provider "aws" {
-      region = "eu-central-1"
-    }
+  }
+  # Terraform binary version constraint
+  required_version = ">= 1.2.0"
+}
+
+
+provider "aws" {
+  region = "eu-central-1"
+}
     
